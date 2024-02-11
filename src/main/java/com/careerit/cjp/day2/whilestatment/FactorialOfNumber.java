da0411d3ae4381106349165ff5de59e5e0b744b3
javawithlwl <javawithlwl@gmail.com>
2024-02-11 06:21:51 +0530

    fixed code and accepted input from user

--- src/main/java/com/careerit/cjp/day2/whilestatment/FactorialOfNumber.java
+++ src/main/java/com/careerit/cjp/day2/whilestatment/FactorialOfNumber.java
@@ -1,16 +1,21 @@
 package com.careerit.cjp.day2.whilestatment;
 
+import java.util.Scanner;
+
 public class FactorialOfNumber {
 
-      public static void main(String[] args){
-            int num = 6;
-            int res = 1;
-            int i = 2;
-            while(i <= num){
-                res = * i;
-                i++;
-            }
-            System.out.println("Factorial of "+num+" is "+res);
-      }
-              
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int num = sc.nextInt();
+        int res = 1;
+        int i = 2;
+        while (i <= num) {
+            res *= i;
+            i++;
+        }
+        System.out.println("Factorial of " + num + " is " + res);
+    }
+
 }
