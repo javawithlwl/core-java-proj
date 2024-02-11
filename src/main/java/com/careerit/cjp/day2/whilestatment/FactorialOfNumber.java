21a7036d5c7dad8c993677d03589231fc198441d
javawithlwl <119342072+javawithlwl@users.noreply.github.com>
2024-02-11 06:19:41 +0530

    Update FactorialOfNumber.java

--- src/main/java/com/careerit/cjp/day2/whilestatment/FactorialOfNumber.java
+++ src/main/java/com/careerit/cjp/day2/whilestatment/FactorialOfNumber.java
@@ -1,4 +1,16 @@
 package com.careerit.cjp.day2.whilestatment;
 
 public class FactorialOfNumber {
+
+      public static void main(String[] args){
+            int num = 6;
+            int res = 1;
+            int i = 2;
+            while(i <= num){
+                res = * i;
+                i++;
+            }
+            System.out.println("Factorial of "+num+" is "+res);
+      }
+              
 }
